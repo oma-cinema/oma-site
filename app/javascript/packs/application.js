022aff865cbd251ae7bf8d924e580275d4f0cdb4
Nicolas Chican <nchican@omacinema.com>
2019-06-23 14:01:20 +0200

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 import "bootstrap";
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 // internal imports
@@ -9,3 +10,7 @@ initMapbox();
 import { initStarRating } from '../plugins/init_star_rating';
 
 initStarRating();
+
+import { initAutocomplete } from '../plugins/init_autocomplete';
+
+initAutocomplete();
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,8 +12,11 @@ const buildMap = () => {
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
       .addTo(map);
   });
 };
@@ -32,5 +35,6 @@ const initMapbox = () => {
     fitMapToMarkers(map, markers);
   }
 };
+
 
 export { initMapbox };
